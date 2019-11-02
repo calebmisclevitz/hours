e97b0be956e15e83f7e7644886fb0567a2458ba1
Caleb Misclevitz <calebmisclevitz@Calebs-MacBook-Pro-2.local>
2019-11-02 13:34:19 -0700

    Dynamically render categories with colors

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,181 @@
 <template>
   <div id="app">
-    <Week msg="Hello from week"/>
-    <ForecastBar/>
+    <Week msg="Hello from week" />
+    <ForecastBar :categories="categories" />
   </div>
 </template>
 
 <script>
-import Week from './components/Week.vue'
-import ForecastBar from './components/ForecastBar.vue'
+import Week from "./components/Week.vue";
+import ForecastBar from "./components/ForecastBar.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Week,
     ForecastBar
   },
-  data () {
+  data() {
     return {
       categories: [
         {
-          name: 'unassigned',
+          name: "Clear",
           hours: [],
-          color: 'transparent'
+          color: "#666"
         },
         {
-          name: 'sleep',
+          name: "Sleep",
           hours: [],
-          color: '#00f'
+          color: "#00f"
         },
         {
-          name: 'work',
+          name: "Work",
           hours: [],
-          color: '#0f0'
+          color: "#0f0"
         },
         {
-          name: 'friends',
+          name: "Friends",
           hours: [],
-          color: '#f00'
+          color: "#f00"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style>
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
 }
 body {
-	line-height: 1;
+  line-height: 1;
 }
-ol, ul {
-	list-style: none;
+ol,
+ul {
+  list-style: none;
 }
-blockquote, q {
-	quotes: none;
+blockquote,
+q {
+  quotes: none;
 }
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
 }
 table {
-	border-collapse: collapse;
-	border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
 * {
   box-sizing: border-box;
 }
 
-html, body {
+html,
+body {
   width: 100%;
   height: 100%;
   padding: 0;
--- src/components/Hour.vue
+++ src/components/Hour.vue
@@ -1,16 +1,16 @@
 <template>
-  <time class="hour"
-       :class="{ 'busy': busy }"
-       @mouseover="handleMouseover"
-       @mousedown="paintForecast"
-       v-bind:datetime="id"
-       >
-  </time>
+  <time
+    class="hour"
+    :class="{ 'busy': busy }"
+    @mouseover="handleMouseover"
+    @mousedown="paintForecast"
+    v-bind:datetime="id"
+  ></time>
 </template>
 
 <script>
 export default {
-  name: 'Hour',
+  name: "Hour",
   props: {
     msg: String,
     index: Number,
@@ -21,61 +21,73 @@ export default {
     armedForecast: String
   },
   methods: {
-    handleMouseover () {
+    handleMouseover() {
       if (this.mouseDown) {
-        this.paintForecast()
+        this.paintForecast();
       }
     },
-    handleHourTouch () {
-      this.paintForecast()
+    handleHourTouch() {
+      this.paintForecast();
     },
-    toggleBusy () {
-      this.isBusy = !this.isBusy
+    toggleBusy() {
+      this.isBusy = !this.isBusy;
     },
-    paintForecast () {
-      this.$emit('paintForecast', this)
+    paintForecast() {
+      this.$emit("paintForecast", this);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .hour {
-  width: .5rem;
-  height: .5rem;
-  border-radius: .5rem;
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 0.5rem;
   background-color: transparent;
   border: 1px solid #444;
   position: relative;
-  animation: pulsedown 600ms cubic-bezier(0.0, 0.0, 0.2, 1);
-  margin-top: .125rem;
+  animation: pulsedown 600ms cubic-bezier(0, 0, 0.2, 1);
+  margin-top: 0.125rem;
 }
 
 .hour:after {
-  content: '';
+  content: "";
   position: absolute;
-  top: -.25rem;
+  top: -0.25rem;
   right: -1rem;
-  bottom: -.25rem;
+  bottom: -0.25rem;
   left: -1rem;
 }
 
 .busy {
   background-color: #eee;
   border: none;
-  animation: pulseup 300ms cubic-bezier(0.4, 0.0, 0.2, 1);;
+  animation: pulseup 300ms cubic-bezier(0.4, 0, 0.2, 1);
 }
 
 @keyframes pulseup {
-  0%   {transform: scale(1)}
-  50% {transform: scale(1.5)}
-  100% {transform: scale(1)}
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 
 @keyframes pulsedown {
-  0%   {transform: scale(1)}
-  50% {transform: scale(.75)}
-  100% {transform: scale(1)}
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.75);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
